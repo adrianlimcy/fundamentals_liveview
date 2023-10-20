fc99fc100d7379fddf0e4fb6cea41a027a6be70f
Adrian Lim <adrianlimcy@gmail.com>
2023-10-20 12:29:45 +0800

    simple form

--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -2,7 +2,8 @@ defmodule DemoWeb.PageLive do
   use DemoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, number: 0)}
+    # {:ok, assign(socket, number: 0, adding_amt_default: 8, form: to_form(%{}))}
+    {:ok, assign(socket, number: 0, form: to_form(%{adding_amt_default: 8}))}
   end
 
   def render(assigns) do
@@ -14,11 +15,15 @@ defmodule DemoWeb.PageLive do
     <%!-- <strong>Hello World!</strong> --%>
     <p>
       <.button phx-click="add">Add</.button>
-    </p>
-
-    <p>
+    <space/>
       <.button phx-click="subtract">Subtract</.button>
     </p>
+
+    <.simple_form for={@form} phx-submit="adding_more">
+      <%!-- <.input field={@form[:add_amount]} value={@adding_amt_default} /> --%>
+      <.input field={@form[:add_amount]} value={@form.params.adding_amt_default} />
+      <.button>Add More</.button>
+    </.simple_form>
 
     <p>
       <%= test("") %>
@@ -40,5 +45,10 @@ defmodule DemoWeb.PageLive do
 
   def handle_event("subtract", _params, socket) do
     {:noreply, assign(socket, number: socket.assigns.number - 1)}
+  end
+
+  def handle_event("adding_more", %{"add_amount" => the_added_amt} , socket) do
+    {num, _reminder} = Integer.parse(the_added_amt)
+    {:noreply, assign(socket, number: socket.assigns.number + num)}
   end
 end
