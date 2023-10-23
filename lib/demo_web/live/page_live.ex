c8e62eedc936e8c02021d5f83d20cba7287ee5c8
Adrian Lim <adrianlimcy@gmail.com>
2023-10-23 15:11:44 +0800

    wrap up

--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -6,38 +6,17 @@ defmodule DemoWeb.PageLive do
     {:ok, assign(socket, number: 0, form: to_form(%{adding_amt_default: 8}))}
   end
 
-  def render(assigns) do
-    ~H"""
-    <%!-- <%= assigns.number %> --%>
-    <%!-- instead of using assigns, you can use @ --%>
-    <%= @number %>
+  # def render(assigns) do
+  #   ~H"""
 
-    <%!-- <strong>Hello World!</strong> --%>
-    <p>
-      <.button phx-click="add">Add</.button>
-    <space/>
-      <.button phx-click="subtract">Subtract</.button>
-    </p>
+  #   """
+  # end
 
-    <.simple_form for={@form} phx-submit="adding_more">
-      <%!-- <.input field={@form[:add_amount]} value={@adding_amt_default} /> --%>
-      <.input field={@form[:add_amount]} value={@form.params.adding_amt_default} />
-      <.button>Add More</.button>
-    </.simple_form>
-
-    <p>
-      <%= test("") %>
-      <%!-- this is a shorthand to execute a local function --%>
-      <.test/>
-    </p>
-    """
-  end
-
-  def test(assigns) do
-    ~H"""
-    This is a test
-    """
-  end
+  # def test(assigns) do
+  #   ~H"""
+  #   This is a test
+  #   """
+  # end
 
   def handle_event("add", _params, socket) do
     {:noreply, assign(socket, number: socket.assigns.number + 1)}
